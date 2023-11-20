8b582938765ceb2229d8859025c9ee9eff2d78ba
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-20 15:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const counttext= document.getElementById('counter');
+const btn=document.getElementById('incrementBtn');
+let count=0;
+function increment() {
+	count++;
+	counttext.textContent= count;
+}
+btn.addEventListener('click', increment);
